ea6fedb703020d6a7a4d9dcf7d36faacb462eeac
joseph99kk <66130768+joseph99kk@users.noreply.github.com>
2025-03-15 14:45:25 +0300

    Add files via upload

---
+++ models.py
@@ -0,0 +1,77 @@
+from django.contrib.auth.models import AbstractUser, Group, Permission
+from django.db import models
+
+
+
+# Create your models here.
+# class user models
+
+class User(AbstractUser):
+    ROLE_CHOICES = (
+        ('student', 'Student'),
+        ('lecturer ','Lecturer'),
+        ('registrar','Registrar'),
+        ('admin','Admin'),
+    )
+    
+    groups = models.ManyToManyField(
+        Group,
+        related_name="custom_user_group",
+        blank=True
+    )
+    user_permissions = models.ManyToManyField(
+        Permission,
+        related_name="custom_user_permissions",
+        blank=True
+    )
+
+    role = models.CharField(max_length=10, choices=ROLE_CHOICES)
+    
+    
+class Admin(models.Model):
+    name = models.CharField(max_length=30)
+    
+
+class Lecturer(models.Model):
+    name = models.CharField(max_length=100)
+    college = models.CharField(max_length=30)
+    issueID = models.TextField()
+    issue = models.CharField(max_length=200)
+    
+    
+    def __str__(self):
+        return self.issue
+    
+class Registrar(models.Model):
+    name = models.CharField(max_length=100)
+    college = models.CharField(max_length=30)
+    issueID = models.IntegerField()
+    issue = models.CharField(max_length=200)
+    date_time = models.DateTimeField(auto_now=True)
+    
+      
+    def __str__(self):
+        return self.issue
+    
+class Course(models.Model):
+    name = models.CharField(max_length=20)
+    
+    def __str__(self):
+        return self.name
+    
+    
+class Student(models.Model):
+    name = models.CharField(max_length=100)
+    college = models.CharField(max_length=30)
+    issue = models.TextField()
+    issueID = models.IntegerField()
+    date = models.DateField(auto_now=True)
+    semester = models.CharField(max_length=20)
+    course = models.ForeignKey(Course, on_delete=models.CASCADE)
+    
+    
+    
+    
+    def __str__(self):
+        return self.issue
+    
