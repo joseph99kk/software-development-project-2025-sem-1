b92b1b742de6e5a1f10e665c117946db8cf014f7
neville03 <akoneville1@gmail.com>
2025-03-31 22:14:42 +0200

    Update App.jsx

--- App.jsx
+++ App.jsx
@@ -24,7 +24,7 @@ const App = () => {
   return (
     <div>
       <StudentDashboard />
-      {/* Pass issues, selectIssue, and notification as props */}
+      {/* Pass issues, selectIssue, and notification as props  */}
       <Dashboard 
         issues={issues} 
         selectIssue={selectIssue} 
