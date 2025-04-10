20f8af662b86e1ebeca49cc0e46ec4a95460f2d3
Mercylina01 <kuteesa.mercylina10@gmail.com>
2025-04-09 23:21:35 -0700

    dashboard

---
+++ src/LecturerDashboard.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const LecturerDashboard = () => {
+  return (
+    <div>
+      <h2>Lecturer Dashboard</h2>
+      <p>List of issues assigned to this lecturer</p>
+    </div>
+  );
+};
+
+export default LecturerDashboard;
