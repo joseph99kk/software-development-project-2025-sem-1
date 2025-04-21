0912bf0022df6190f6ae2975203eaa739b38f865
Mercylina01 <kuteesa.mercylina10@gmail.com>
2025-04-21 06:23:46 -0700

    Update LecturerDashboard.jsx
    designed the lecturers dashboard

--- src/LecturerDashboard.jsx
+++ src/LecturerDashboard.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
+import .'/LecturerDashboard.css';
 
 const LecturerDashboard = () => {
+
   return (
-    <div>
+    <div className ="lecturer-container">
       <h2>Lecturer Dashboard</h2>
       <p>List of issues assigned to this lecturer</p>
     </div>
