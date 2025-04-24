d7f8d7110e080c24670757b909026569381fdeb6
Mercylina01 <kuteesa.mercylina10@gmail.com>
2025-04-24 16:56:54 -0700

    Update WelcomePage.js

--- src/WelcomePage.js
+++ src/WelcomePage.js
@@ -6,7 +6,7 @@ function WelcomePage() {
     <div className="welcome-page">
       <h1>Welcome to the University Issue Submission  Portal</h1>
       <p className="welcome-message">
-      Thank you for visiting . Click Below to proceed with Registration:</p>
+       Click Below to proceed with Registration:</p>
       <Link to="/register" className="register-button">Register</Link>
       <p>If you already have an account, please login:</p>
         <Link to="/login" className="login-button">Login</Link>
